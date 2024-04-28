6d082af253c26da61d171b104de664640d1f26fd
t4top <temitope@yahoo.co.jp>
2024-04-28 10:41:31 +0200

    Implement basic UI for the demo wallet

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,21 @@
-<h1>Mina Demo Wallet</h1>
+<script lang="ts">
+	import { shortenAddress } from '@mina-wallet-adapter/core';
+	import { account } from '$lib/adapter';
+	const { label, icon, address } = account;
+</script>
+
+<div>
+	<img src={icon} alt={`${label} icon`} />
+	<h1>{label}</h1>
+</div>
+<div>
+	<p>
+		This is a reference implementation of a Mina Wallet supporting Wallet Standard. It showcases how
+		the <b>Mina Wallet Adapter</b> can be easily integrated into existing or new wallets within the Mina
+		Protocol ecosystem.
+	</p>
+	<p>
+		<strong>Wallet Address:</strong>
+		{shortenAddress(address)}
+	</p>
+</div>
